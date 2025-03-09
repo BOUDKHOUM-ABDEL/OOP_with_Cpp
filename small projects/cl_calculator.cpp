641a264c83e3a930d1575aab30865e8e7ce9244f
BOUD__ABDEL <boudkhoum.abdellah.code@gmail.com>
2025-03-09 23:29:57 +0000

    function add

--- small projects/cl_calculator.cpp
+++ small projects/cl_calculator.cpp
@@ -1,1 +1,13 @@
-include <
+#include <iostrem>
+#include <string>
+
+class clcalculator {
+
+public :
+	int total=0;
+
+	int add(int val) {
+		total += val;
+		cout << "the total after adding" << val << "is" << total;
+	}
+};
