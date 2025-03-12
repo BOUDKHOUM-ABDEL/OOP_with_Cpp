65048f35d79c2f14b77b50f11cabf788f95eafc5
BOUD__ABDEL <boudkhoum.abdellah.code@gmail.com>
2025-03-12 13:27:12 +0000

    Create EX2.cpp

---
+++ Exercice in C++/EX2.cpp
@@ -0,0 +1,7 @@
+/* Écrivez une fonction générique contient_element utilisant un template qui vérifie si un
+ tableau contient un élément donné.
+ 1. La fonction doit être définie avec un template.
+ 2. La fonction doit fonctionner avec n’importe quel type (int, float, double, string, etc.).
+ 3. La fonction retourne true si l’élément est trouvé, sinon false.
+ 4. L’allocation dynamique de mémoire avec new et la libération de mémoire avec delete
+ doivent être effectuées dans le main.*/
