0e049e55c383208af5e0b2c9e60df002e445c6ae
BOUD__ABDEL <boudkhoum.abdellah.code@gmail.com>
2025-03-15 21:30:22 +0000

    add exercice text

---
+++ Exercice in C++/EX3.cpp
@@ -0,0 +1,7 @@
+/* L’héritage permet à une classe de réutiliser les caractéristiques d’une autre.
+1. Quelle est la différence entre une classe mère et une classe fille?
+2. Dans un centre de formation, identifiez une classe mère et deux classes filles.
+3. Quels attributs et méthodes seraient communs à toutes les classes?
+4. Quels attributs et méthodes seraient spécifiques à chaque classe fille?
+5. Pourquoi l’héritage est-il utile pour éviter la redondance dans un programme?
+6. Dans quels cas l’héritage peut-il être mal utilisé ou causer des problèmes de conception? */
