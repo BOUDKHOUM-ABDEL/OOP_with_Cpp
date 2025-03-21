07e8d9ecf16e45f9b4bf28ed48ec7f37ea981073
BOUD__ABDEL <boudkhoum.abdellah.code@gmail.com>
2025-03-21 23:58:59 +0000

    create class

---
+++ small projects/cl_person.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class  clsPerson {
+private:
+	int _id;
+	string _nom;
+	string _prenom;
+	string _phone;
+	string _email;
+
+};
+
+int main() {
+	
+	system("pause");
+
+
+
+}
