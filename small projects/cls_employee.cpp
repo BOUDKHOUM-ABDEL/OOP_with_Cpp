850534c0a70454a494ed6bf9bb047c773759efc3
BOUD__ABDEL <boudkhoum.abdellah.code@gmail.com>
2025-03-23 00:08:27 +0000

    Update cls_employee.cpp

--- small projects/cls_employee.cpp
+++ small projects/cls_employee.cpp
@@ -1,1 +1,17 @@
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+class  clsEmployee {
+private:
+	int _id;
+	string _nom;
+	string _prenom;
+	string _phone;
+	string _email;
+	float _salary;
+		
+
+}
+
