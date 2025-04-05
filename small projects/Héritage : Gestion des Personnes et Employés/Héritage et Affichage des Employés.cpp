477ea8cacf8331001f6f17b228565fbd6c4171a2
BOUD__ABDEL <boudkhoum.abdellah.code@gmail.com>
2025-04-05 22:48:15 +0000

    add clsemployee

--- small projects/Héritage : Gestion des Personnes et Employés/Héritage et Affichage des Employés.cpp
+++ small projects/Héritage : Gestion des Personnes et Employés/Héritage et Affichage des Employés.cpp
@@ -9,6 +9,47 @@ private:
     string _prenom;
     string _phone;
     string _email;
+public:
+    clsPerson(int id, string nom, string prenom, string phone, string email) {
+        _id = id;
+        _nom = nom;
+        _prenom = prenom;
+        _phone = phone;
+        _email = email;
+    }
+    string fullname() {
+        return _nom + " " + _prenom;
+    }
+    void print() {
+        cout << "_______________info______________" << endl;
+        cout << "ID: " << _id << endl;
+        cout << "Nom: " << _nom << endl;
+        cout << "Prenom: " << _prenom << endl;
+        cout << "fullname: " << fullname() << endl;
+        cout << "Phone: " << _phone << endl;
+        cout << "Email: " << _email << endl;
+        cout << "________________________________" << endl;
+    }
+    void Sendemail(string subject, string body) {
+        cout << "the following message sent successfully to email : " << _email << endl;
+        cout << "subject :" << subject << endl;
+        cout << "body : " << body << endl;
+        cout << endl;
+    }
+
+    void Sendsms(string sms) {
+        cout << "the following message sent successfully :" << _phone << endl;
+        cout << sms << endl;
+        cout << endl;
+    }
+};
+
+class clsEmployee : public clsPerson {
+private:
+
+    string _title;
+    int _salary;
+    string _departement;
 
 };
 
