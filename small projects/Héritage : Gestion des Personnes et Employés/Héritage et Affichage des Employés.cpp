ab663463c2bbdcc21bb2bcb254c538ae6e4fb0ce
BOUD__ABDEL <boudkhoum.abdellah.code@gmail.com>
2025-04-05 22:46:26 +0000

    Update Héritage et Affichage des Employés.cpp

--- small projects/Héritage : Gestion des Personnes et Employés/Héritage et Affichage des Employés.cpp
+++ small projects/Héritage : Gestion des Personnes et Employés/Héritage et Affichage des Employés.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+class clsPerson {
+private:
+    int _id;
+    string _nom;
+    string _prenom;
+    string _phone;
+    string _email;
+
+};
+
+int main() {
+    return 0;
+}
