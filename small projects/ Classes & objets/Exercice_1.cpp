69f46f59b882a1f98d6b8d227f7e576c9596e2ff
BOUD__ABDEL <boudkhoum.abdellah.code@gmail.com>
2025-04-08 13:42:04 +0100

    Create Exercice_1.cpp

---
+++ small projects/ Classes & objets/Exercice_1.cpp
@@ -0,0 +1,9 @@
+/* Exercice 1 :
+    Définissez une classe Person avec les attributs suivants :
+     — nom (chaîne de caractères) → private
+     — âge (entier) → private
+    Ajoutez :
+      — Unconstructeur public pour initialiser les attributs.
+      — Une méthode introduire() → public qui affiche :
+       "Bonjour, je m'appelle nom et j'ai âge ans."
+     Créez un objet de cette classe et appelez la méthode.*/
