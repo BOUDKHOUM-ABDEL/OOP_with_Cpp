369afeb1cebea5115da1c6974fb4629543a34fdd
BOUD__ABDEL <boudkhoum.abdellah.code@gmail.com>
2025-04-13 00:49:26 +0100

    Update Exercice_2.cpp

--- small projects/ Classes & objets/Exercice_2.cpp
+++ small projects/ Classes & objets/Exercice_2.cpp
@@ -8,9 +8,20 @@ class clsEtution
 private:
     vector <float>notes;
 
+
 public:
 
-    
+    float obtenirMoyenne() {
+        if (notes.empty()) return 0;
+
+        float somme = 0;
+        for (int note : notes) {
+            somme += note;
+        }
+        return somme / notes.size();
+    }
+
+   
 };
 
 int main()
