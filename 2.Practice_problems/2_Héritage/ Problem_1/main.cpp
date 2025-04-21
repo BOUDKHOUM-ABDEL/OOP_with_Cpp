b705597aafefc69aedca5a83409f2241301c10b2
BOUDKHOUM-ABDEL <boudkhoum.abdellah.code@gmail.com>
2025-04-21 14:34:28 +0100

    create class Animal

---
+++ 2.Practice_problems/2_Héritage/ Problem_1/main.cpp
@@ -0,0 +1,17 @@
+#include <isostream>
+using namespace std;
+class clsAnimal {
+
+public:
+    void son() {
+        cout << "Son d’un animal inconnu." << endl;
+    }
+};
+
+
+ 
+};
+int main() {
+
+	return 0;
+}
