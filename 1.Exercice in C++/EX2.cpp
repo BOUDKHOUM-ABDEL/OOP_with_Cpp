dcd16a7ba8091ec5d6aba6865610240812218464
BOUDKHOUM-ABDEL <boudkhoum.abdellah.code@gmail.com>
2025-04-25 00:36:23 +0100

    add exercice 2

--- 1.Exercice in C++/EX2.cpp
+++ 1.Exercice in C++/EX2.cpp
@@ -1,52 +1,21 @@
-/* Écrivez une fonction générique contient_element utilisant un template qui vérifie si un
- tableau contient un élément donné.
- 1. La fonction doit être définie avec un template.
- 2. La fonction doit fonctionner avec n’importe quel type (int, float, double, string, etc.).
- 3. La fonction retourne true si l’élément est trouvé, sinon false.
- 4. L’allocation dynamique de mémoire avec new et la libération de mémoire avec delete
- doivent être effectuées dans le main.*/
- #include <iostream>
-
+#include <iostream>
 using namespace std;
-
-template <class T>
-bool contient_element(T* tableau, int taille, T element) {
-    for (int i = 0; i < taille; i++) {
-        if (tableau[i] == element) {
-            return true; 
-        }
-    }
-    return false; 
-}
-
 int main() {
-    int taille;
-    int elementRecherche;
+	int i;
+	bool estPair;
+	string type;
 
+	for (i = 1; i <= 10; i++) {
+		estPair = (i % 2 == 0);
 
-    cout << "Entrez la taille du tableau : ";
-    cin >> taille;
+		if (estPair) {
+			type = "pair";
+		}
+		else {
+			type = "impair";
+		}
+		cout << "L'entier " << i << " est " << type << "." << endl
 
-
-    int* tableau = new int[taille];
-
-    cout << "Entrez les elements du tableau : " << endl;
-    for (int i = 0; i < taille; i++) {
-        cin >> tableau[i];
-    }
-
-    cout << "Entrez l'element à rechercher : ";
-    cin >> elementRecherche;
-
-    if (contient_element(tableau, taille, elementRecherche)) {
-        cout << "L'element " << elementRecherche << " est present dans le tableau." << endl;
-    }
-    else {
-        cout << "L'element " << elementRecherche << " n'est pas present dans le tableau." << endl;
-    }
-
-    delete[] tableau;
-
-    return 0;
+	}
+	return 0;
 }
-
