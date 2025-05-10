a7383313511b496639745c124e6ebac6010ce60b
BOUDKHOUM-ABDEL <boudkhoum.abdellah.code@gmail.com>
2025-05-10 17:55:14 +0100

    ajouter trois classes dérivées

--- 2.Practice_problems/2_Héritage/Problem 2/clsAppareil.h
+++ 2.Practice_problems/2_Héritage/Problem 2/clsAppareil.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 using namespace std;
+
 class clsAppareil {
 private: 
 	int _id;
@@ -12,10 +13,46 @@ public:
 	}
 
 	void afficherStatut() {
-		cout << "Appareil : "<< _id << " : " << _statut
+		cout << "Appareil : " << _id << " : " << _statut ;
 	}
 };
-//Modélisez un système domotique avec héritage et liste d’initialisation.Créez une classe de
-//base Appareil avec deux attributs protégés : id(entier) et statut(chaîne de caractères).
-//Le constructeur de Appareil utilise une liste d’initialisation pour définir ces attributs.La
-//méthode afficherStatut() affiche le message suivant : Appareil #ID : STATUT.
+
+class clsLumiere : public clsAppareil{
+private: 
+	int _intensite;
+public:
+	clsLumiere(int id, string statut, int intensite) :clsAppareil(_id, _statut) {
+		_intensite = intensite;
+	}
+	void afficher() {
+		afficherStatut();
+			cout << "Température actuelle : " << temperature << "°C" << endl;
+    }
+	}
+};
+
+class clsThermostat : protected clsAppareil {
+private:
+	float _temperature;
+public:
+	clsThermostat (int id, string statut, float temperature) :clsAppareil(_id, _statut) {
+		_temperature = temperature;
+	}
+	void afficher() {
+		afficherStatut();
+		cout << "Température actuelle : " << temperature << "°C" << endl;
+	}
+	}
+};
+
+class clsCamera : private clsAppareil {
+private:
+	int _intensite;
+public:
+	clsCamera(int id, string statut, int resolution) :clsAppareil(_id, _statut) {
+		_resolution = resolution;
+	}
+	void afficherStatut() {
+		cout << "Appareil : " << _id << " : " << _statut;
+	}
+};
