9dcca183890ca0416a20812cee91de93ae3961c5
BOUDKHOUM-ABDEL <boudkhoum.abdellah.code@gmail.com>
2025-05-10 18:33:58 +0100

    final files

--- 2.Practice_problems/2_Héritage/Problem 2/main.cpp
+++ 2.Practice_problems/2_Héritage/Problem 2/main.cpp
@@ -5,10 +5,10 @@ int main() {
 
 	 clsLumiere l(1, "actif", 75);
 	 clsThermostat t(2, "en veille", 22.5);
-	 clsCamera c(3, "hors ligne", "1080p");
+	/* clsCamera c(3, "hors ligne", "1080p");*/
 	 l.afficher();
 	 t.afficher();
-	 c.afficher();
+	/* c.afficher();*/
 	return 0;
 }
 
