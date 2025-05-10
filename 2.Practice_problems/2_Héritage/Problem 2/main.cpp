54e35601d88c1da2612b9a345317bbf8432e3e81
BOUDKHOUM-ABDEL <boudkhoum.abdellah.code@gmail.com>
2025-05-10 18:15:40 +0100

    create objects

--- 2.Practice_problems/2_Héritage/Problem 2/main.cpp
+++ 2.Practice_problems/2_Héritage/Problem 2/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
-#include 'clsAppareil.h'
+#include "clsAppareil.h"
 using namespace std;
 int main() {
 
-
+	 clsLumiere l(1, "actif", 75);
+	 clsThermostat t(2, "en veille", 22.5);
+	 clsCamera c(3, "hors ligne", "1080p");
 	return 0;
 }
 
